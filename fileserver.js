31cf63ef409a9a00d835d4da67a36a949b8613f7
ayeshaali <aliayeshanyc@gmail.com>
2019-01-09 17:20:25 -0500

    message

---
+++ fileserver.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var fs = require('fs');
+const PORT = process.env.PORT || 5000
+
+var app = express();
+app.use(express.static('public'));
+app.listen(PORT, function(){
+  console.log('Easy server listening for requests on port'+ PORT+'!');
+});
+
+//help
+
+app.get('/', function(request, response){
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  var file = fs.readFileSync("index.html");
+  response.write(file);
+  response.write(file);
+  response.send();
+});
